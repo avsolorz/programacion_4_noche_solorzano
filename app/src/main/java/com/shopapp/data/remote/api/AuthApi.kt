3bddece6a1451028c08e677b5c73d21636f53a0e
avsolorz <victoriasolorzano62@gmail.com>
2026-06-14 11:33:33 -0500

    13.2 Endpoints en AuthApi.kt

--- app/src/main/java/com/shopapp/data/remote/api/AuthApi.kt
+++ app/src/main/java/com/shopapp/data/remote/api/AuthApi.kt
@@ -18,4 +18,10 @@ interface AuthApi {
 
     @POST("auth/logout/")
     suspend fun logout(@Body body: LogoutRequest): Response<Unit>
+
+    @POST("auth/password-reset/")
+    suspend fun requestPasswordReset(@Body body: PasswordResetRequestDto): Response<MessageDto>
+
+    @POST("auth/password-reset/confirm/")
+    suspend fun confirmPasswordReset(@Body body: PasswordResetConfirmDto): Response<MessageDto>
 }
